9f2e3403112ff6df15ac3168b54922086d348363
bpppoh <ponlawat.changto@gmail.com>
2026-05-07 10:37:08 +0700

    add Prodect and BaseEntity Classes

--- Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f273127779feb8a17fbc2be7e969fbce5b56487")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
