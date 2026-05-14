cbfd22cdf095fb840a17eb64565b5ab8bbf3b4bb
bpppoh <ponlawat.changto@gmail.com>
2026-05-14 07:49:30 +0700

    Etc auto change

--- Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51b1815e9baa3322275e13792ed2ccd70a8dc42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d4ff4c7769960f4cc73001f80597dcf0aa26a85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
